c09a6522eca4d053184f0e67f44b49d6eef65e00
Krish Kumar <indokrisgraphics@gmail.com>
2025-07-28 22:00:33 +0530

    All working without media

--- firstframe/memories/urls.py
+++ firstframe/memories/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from . import views
 
+from django.urls import path
+from .views import home, about_me, memories, gallery, thank_you
+
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', home, name='home'),
+    path('about/', about_me, name='about_me'),
+    path('memories/', memories, name='memories'),
+    path('gallery/', gallery, name='gallery'),
+    path('thank-you/', thank_you, name='thank_you'),
 ]
+
--- firstframe/memories/views.py
+++ firstframe/memories/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'memories/home.html')
+
+def about_me(request):
+    return render(request, 'memories/about_me.html')
+
+def memories(request):
+    return render(request, 'memories/memories.html')
+
+def gallery(request):
+    return render(request, 'memories/gallery.html')
+
+def thank_you(request):
+    return render(request, 'memories/thank_you.html')
